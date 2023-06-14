355fd9786689f5faa69dc95aeed82fdf34515b06
“NicholasAntonio” <nicholas.nkt.antonio@gmail.com>
2023-06-14 19:29:31 -0300

    script adjust

--- js/script.js
+++ js/script.js
@@ -6,22 +6,20 @@ menuIcon.onclick = () => {
   navbar.classList.toggle("active");
 };
 
-let section = document.querySelectorAll("section");
+let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-  section.forEach((sec) => {
+  sections.forEach(sec=> {
     let top = window.scrollY;
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      navLinks.forEach((links) => {
+      navLinks.forEach(links => {
         links.classList.remove("active");
-        document
-          .querySelector("header nav a[href*= " + id + "]")
-          .classList.add("active");
+        document.querySelector("header nav a[href*= " + id + "]").classList.add("active");
       });
     }
   });
@@ -30,8 +28,8 @@ window.onscroll = () => {
 
   header.classList.toggle("sticky", window.scrollY > 100);
 
-  let menuIcon = document.querySelector("#menu-icon");
-  let navbar = document.querySelector(".navbar");
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.toggle('active');
 };
 
 ScrollReveal({
